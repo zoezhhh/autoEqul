e0ba6953191a0f577a56578b42496997212f1395
ZZhu <zoezhu0807@gmail.com>
2022-08-19 20:14:52 -0400

    updated SAVED_NIS, SAVED_EXCEL

--- src/config.py
+++ src/config.py
@@ -14,10 +14,10 @@ INPUT_FILE = r"R:\SALESAPP\Explainer Tool\UL\Excel Tool\Testing\Zoe\Test Cases V
 INPUT_SHEET = "Sheet1"
 
 # path to the saved NIS file
-SAVED_NIS = r"R:\SALESAPP\Explainer Tool\UL\Excel Tool\Testing\Zoe\Python tool\Test Cases NIS\equl.NIS"
+SAVED_NIS = r"\\eusers.equitable.int\desktops$\zzhu\Documents\equl.NIS"
 
 # path to the exported Excel file
-EXPORTED_EXCEL = r"R:\SALESAPP\Explainer Tool\UL\Excel Tool\Testing\Zoe\Python tool\Test Cases Excel\equl.xls"
+EXPORTED_EXCEL = r"C:\Users\zzhu\AppData\Local\Temp\equl.xls"
 
 # path to the working folder
 WORKSPACE = "generated"
